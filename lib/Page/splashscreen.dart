1779465ac5985a50dc8f0ad4c3dbbe404004f7bd
Delbert <delbertmelvin1209@gmail.com>
2024-11-17 23:17:15 +0700

    first commit

---
+++ lib/Page/splashscreen.dart
@@ -0,0 +1,65 @@
+import 'package:aol_mcc/Page/AuthPage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:aol_mcc/Page/homePage.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin{
+  @override
+
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+    Future.delayed(Duration(seconds: 30), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (_) => AuthPage()
+      ));
+    });
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: SystemUiOverlay.values);
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height; //buat screen height tapi pake persentase dari screen
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF777777), Color(0xFF333333)],
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('lib/Assets/splashScreen.png'),
+            SizedBox(height: screenHeight * 0.02),
+            Text(
+              'BANBOO TEAMS',
+              style: TextStyle(
+                fontFamily: 'Bangers',
+                fontSize: 40,
+                color: Colors.white70
+              )
+            )
+          ],
+        )
+      ),
+    );
+  }
+}
+
